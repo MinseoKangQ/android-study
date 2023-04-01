b83eee7ec07a1e01caabb7743c08b5d6109469df
MinseoKangQ <kms02171@naver.com>
2023-04-02 00:45:32 +0900

    Add week5 android project

---
+++ week5/app/src/main/java/com/first/week5/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.first.week5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.test_layout)
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            Snackbar.make(it, "Clicked!", Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+}
