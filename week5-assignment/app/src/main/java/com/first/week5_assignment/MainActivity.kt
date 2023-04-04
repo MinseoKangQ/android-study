7f4e1feb9851a9a2ca0c86a50ce44f70b7bfe1a4
MinseoKangQ <kms02171@naver.com>
2023-04-04 13:25:42 +0900

    Edit week5-assignment project

--- week5-assignment/app/src/main/java/com/first/week5_assignment/MainActivity.kt
+++ week5-assignment/app/src/main/java/com/first/week5_assignment/MainActivity.kt
@@ -2,10 +2,14 @@ package com.first.week5_assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val likeBtn = findViewById<Button>(R.id.button)
+        likeBtn.setOnClickListener { println("Like clicked!") }
     }
 }
