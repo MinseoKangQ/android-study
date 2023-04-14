de836ef664f57515578c44ec69517f0bdac3f9cd
MinseoKangQ <kms02171@naver.com>
2023-04-15 02:14:59 +0900

    Use LiveData, fun

--- week7-assignment/app/src/main/java/com/project/week7_assignment/MainActivity.kt
+++ week7-assignment/app/src/main/java/com/project/week7_assignment/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.project.week7_assignment
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,10 +19,15 @@ class MainActivity : AppCompatActivity() {
         val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             val msg = it.data?.getStringExtra("resultdata") ?: ""
             Snackbar.make(findViewById(R.id.button), msg, Snackbar.LENGTH_SHORT).show()
+            findViewById<EditText>(R.id.editText).setText(msg)
         }
 
-        // editText의 값 받아고기
-        val value = findViewById<TextView>(R.id.editText)
+        // editText의 값 받아오기
+        // 이 부분 잘 안됨
+        // Intent로 넘기기
+        val editText = findViewById<EditText>(R.id.editText)
+        println("###################${editText.text}")
+
 
         // 버튼 누르면 SecondActivity 시작
         findViewById<Button>(R.id.button)?.setOnClickListener {
--- week7-assignment/app/src/main/java/com/project/week7_assignment/SecondActivity.kt
+++ week7-assignment/app/src/main/java/com/project/week7_assignment/SecondActivity.kt
@@ -3,9 +3,11 @@ package com.project.week7_assignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
+import androidx.lifecycle.ViewModelProvider
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +15,9 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
 
         // MainActivity 에서 데이터 받아오기
+        // Intent로 전달 받은 숫자를 표시
+        // 이 부분 수정해야 함
         val data = intent?.getStringExtra("inputdata") ?: ""
-
         findViewById<TextView>(R.id.textView).text = "${data}"
 
         // 데이터 되돌려주기
@@ -26,11 +29,28 @@ class SecondActivity : AppCompatActivity() {
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 val resultIntent = Intent()
-                resultIntent.putExtra("resultdata", "result string")
+                resultIntent.putExtra("resultdata", "${findViewById<TextView>(R.id.textView).text}")
                 setResult(RESULT_OK, resultIntent)
                 finish()
             }
 
         })
+
+        // 뷰 모델 객체 가져오기
+        val viewModel = ViewModelProvider(this, MyViewModel2Factory(10))[MyViewModel::class.java]
+        viewModel.countLiveData.observe(this){
+            findViewById<TextView>(R.id.textView).text = "${it}"
+        }
+
+        // 버튼 누르면 증가
+        findViewById<Button>(R.id.buttonInc)?.setOnClickListener {
+            viewModel.increaseCount()
+        }
+
+        // 버튼 누르면 감소
+        findViewById<Button>(R.id.buttonDec)?.setOnClickListener {
+            viewModel.decreaseCount()
+        }
+
     }
 }
