510a46d83b0e1fafefc454194a41d21ee2d3d560
MinseoKangQ <kms02171@naver.com>
2023-09-22 11:30:37 +0900

    week5 added

---
+++ advanced-mobile-programming/week5/app/src/main/java/com/example/week5/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.week5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val db = MyDatabase.getDatabase(this)
+        val dao = db.getMyDao()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            dao.insertStudent(Student(1, "james"))
+            dao.insertStudent(Student(2, "top"))
+        }
+
+        val allStudents = dao.getAllStudents()
+        allStudents.observe(this) {
+            val str = StringBuilder().apply {
+                for((id, name) in it) {
+                    append(id)
+                    append("-")
+                    append(name)
+                    append("\n")
+                }
+            }.toString()
+
+            val textView = findViewById<TextView>(R.id.textView)
+            textView.text = str
+        }
+
+        findViewById<Button>(R.id.insert).setOnClickListener {
+            val id = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+            val name = findViewById<EditText>(R.id.editTextTextPersonName2).text.toString()
+            CoroutineScope(Dispatchers.IO).launch {
+                dao.insertStudent(Student(id.toInt(), name))
+            }
+        }
+
+        findViewById<Button>(R.id.delete).setOnClickListener {
+            val id = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+            CoroutineScope(Dispatchers.IO).launch {
+                dao.deleteStudent(Student(id.toInt(), ""))
+            }
+        }
+
+    }
+}
---
+++ advanced-mobile-programming/week5/app/src/main/java/com/example/week5/MyDao.kt
@@ -0,0 +1,22 @@
+package com.example.week5
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MyDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(student: Student)
+
+    @Delete
+    suspend fun deleteStudent(student: Student)
+
+    @Query("SELECT * from student_table")
+    fun getAllStudents(): LiveData<List<Student>>
+
+}
